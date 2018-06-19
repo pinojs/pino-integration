a148295f688f60527d8206d3c745d2a9495c2929
davidmarkclements <huperekchuno@googlemail.com>
2018-06-19 17:33:38 +0200

    functioning

--- setup.js
+++ setup.js
@@ -3,6 +3,7 @@ const { join } = require('path')
 const { mkdirSync } = require('fs')
 const { execSync, spawn } = require('child_process')
 const branch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim()
+const { print } = require('easy-table')
 const { pino, peers } = require('./config')
 const REPOS = join(__dirname, 'repos')
 try { execSync(`rm -rf ${REPOS}`) } catch (e) {}
@@ -11,14 +12,29 @@ process.chdir(REPOS)
 console.log(`cloning pino and switching to ${branch} branch`)
 execSync(`git clone ${pino}`, {stdio: 'ignore'})
 execSync(`git checkout ${branch}`, {cwd: join(REPOS, 'pino')}, {stdio: 'ignore'})
-console.log(`installing pino dependencies`)
-execSync(`npm install`, {cwd: join(REPOS, 'pino')}, {stdio: 'ignore'})
-peers.forEach(({name, url}) => {
-  check(name, spawn('git', ['clone', url]))
+const results = peers.reduce((o, {name}) => Object.assign({
+  [name]: { library: name, current: false, branch: false, compare: ''}  
+}, o), {})
+var count = peers.length
+const values = Object.values || ((o) => Object.keys(o).map((k) => o[k]))
+peers.forEach(async ({name, url}) => {
+  await check(name, url)
+  count--
+  if (count === 0) {
+    const table = values(results).map(({library, current, branch, compare}) => ({
+      Library: library, 
+      Current: current ? '   ✅' : '   ❌', 
+      Branch: branch ? ' ✅' : ' ❌',
+      'Branch Test Comparison': compare
+    }))
+    console.log()
+    console.log(print(table))
+  }
 })
-process.exitCode = 0
-async function check (name, sp) {
-  const cloneSuccess = await once(sp, 'close') === 0
+async function check (name, url) {
+  console.log(`cloning ${name} repository`)
+  const clone = spawn('git', ['clone', url])
+  const cloneSuccess = await once(clone, 'close') === 0
   if (cloneSuccess === false) {
     console.error(`Fail: Unable to clone ${name}`)
     process.exitCode = 1
@@ -26,35 +42,55 @@ async function check (name, sp) {
   }
   console.log(`${name} cloned, installing dependencies`)
   const install = spawn('npm', ['install'], {cwd: join(REPOS, name), stdio: 'ignore'})
-  const installSuccess = await once(install, 'close') === 0
-  if (installSuccess === false) {
+  const installed = await once(install, 'close') === 0
+  if (installed === false) {
     console.error(`Fail: ${name} unable to install dependencies`)
     process.exitCode = 1
     return
   }
-  console.log(`${name} dependencies installed, running local tests`)
+  console.log(`${name} dependencies installed, running current tests`)
   const test = spawn('npm', ['test'], {cwd: join(REPOS, name), stdio: 'ignore'})
-  const testSuccess = await once(test, 'close') === 0
-  if (testSuccess === false) {
-    console.error(`Fail: ${name} failed local test!`)
+  const tested = await once(test, 'close') === 0
+  if (tested === false) {
+    console.error(`Fail: ${name} failed current test!`)
+    process.exitCode = 1
+  } else results[name].current = true
+  console.log(`${name} current tests passed, attempt switch to equivalent branch`)
+  const checkout = await spawn('git', ['checkout', branch], {cwd: join(REPOS, name), stdio: 'ignore'})
+  await once(checkout, 'close')
+  const repoBranch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim()
+  results[name].compare = `pino ${branch} <–> ${name} ${repoBranch} `
+  console.log(`${name} is on ${repoBranch}, reinstalling dependencies`)
+  const removeModules = spawn('rm', ['-fr', 'node_modules'], {cwd: join(REPOS, name), stdio: 'ignore'})
+  const modulesRemoved = await once(removeModules, 'close') === 0
+  if (modulesRemoved === false) {
+    console.error(`Fail: ${name} unable to reinstall dependencies for ${repoBranch}, could not remove node_modules`)
+    process.exitCode = 1
+    return
+  } 
+  const reinstall = spawn('npm', ['install'], {cwd: join(REPOS, name), stdio: 'ignore'})
+  const reinstalled = await once(reinstall, 'close') === 0
+  if (reinstalled === false) {
+    console.error(`Fail: ${name} unable to reinstall dependencies for ${repoBranch}`)
     process.exitCode = 1
     return
   }
-  console.log(`${name} local tests passed, running integration tests`)
-  const link = spawn('npm', ['link', join(REPOS, 'pino')], {cwd: join(REPOS, name), stdio: 'ignore'})
-  const linkSuccess = await once(link, 'close') === 0
-  if (linkSuccess === false) {
-    console.error(`Fail: ${name} could not link pino!`)
+  console.log(`${name} dependencies reinstalled, running integration tests`)
+  const link = spawn('npm', ['install', join(REPOS, 'pino')], {cwd: join(REPOS, name), stdio: 'ignore'})
+  const linked = await once(link, 'close') === 0
+  if (linked === false) {
+    console.error(`Fail: ${name} could not install pino from ${branch}!`)
     process.exitCode = 1
     return
   }
   const integration = spawn('npm', ['test'], {cwd: join(REPOS, name), stdio: 'ignore'})
-  const integrationSuccess = await once(test, 'close') === 0
-  if (integrationSuccess === false) {
+  const integrated = await once(integration, 'close') === 0
+  if (integrated === false) {
     console.error(`Fail: ${name} failed integration test!`)
     process.exitCode = 1
     return
   }
+  results[name].branch = true
   console.log(`Success: ${name} passed integration tests`)
 }
 
