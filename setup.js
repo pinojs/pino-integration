eddee047d3406473e44c725e27af20a85fca5a98
davidmarkclements <huperekchuno@googlemail.com>
2018-06-20 13:06:32 +0200

    ci fix

--- setup.js
+++ setup.js
@@ -34,7 +34,7 @@ peers.forEach(async ({name, url}) => {
 async function check (name, url) {
   console.log(`cloning ${name} repository`)
   const clone = spawn('git', ['clone', url])
-  const cloneSuccess = await once(clone, 'close') === 0
+  const cloneSuccess = ~~(await once(clone, 'close')) === 0
   if (cloneSuccess === false) {
     console.error(`Fail: Unable to clone ${name}`)
     process.exitCode = 1
@@ -42,7 +42,7 @@ async function check (name, url) {
   }
   console.log(`${name} cloned, installing dependencies`)
   const install = spawn('npm', ['install'], {cwd: join(REPOS, name), stdio: 'ignore'})
-  const installed = await once(install, 'close') === 0
+  const installed = ~~(await once(install, 'close')) === 0
   if (installed === false) {
     console.error(`Fail: ${name} unable to install dependencies`)
     process.exitCode = 1
@@ -50,7 +50,7 @@ async function check (name, url) {
   }
   console.log(`${name} dependencies installed, running current tests`)
   const test = spawn('npm', ['test'], {cwd: join(REPOS, name), stdio: 'ignore'})
-  const tested = await once(test, 'close') === 0
+  const tested = ~~(await once(test, 'close')) === 0
   if (tested === false) {
     console.error(`Fail: ${name} failed current test!`)
     process.exitCode = 1
@@ -63,14 +63,14 @@ async function check (name, url) {
   if (repoBranch !== 'master') {
     console.log(`${name} is on ${repoBranch}, reinstalling dependencies`)
     const removeModules = spawn('rm', ['-fr', 'node_modules'], {cwd: join(REPOS, name), stdio: 'ignore'})
-    const modulesRemoved = await once(removeModules, 'close') === 0
+    const modulesRemoved = ~~(await once(removeModules, 'close')) === 0
     if (modulesRemoved === false) {
       console.error(`Fail: ${name} unable to reinstall dependencies for ${repoBranch}, could not remove node_modules`)
       process.exitCode = 1
       return
     }
     const reinstall = spawn('npm', ['install'], {cwd: join(REPOS, name), stdio: 'ignore'})
-    const reinstalled = await once(reinstall, 'close') === 0
+    const reinstalled = ~~(await once(reinstall, 'close')) === 0
     if (reinstalled === false) {
       console.error(`Fail: ${name} unable to reinstall dependencies for ${repoBranch}`)
       process.exitCode = 1
@@ -89,7 +89,7 @@ async function check (name, url) {
   }
   console.log(`${name} linked to pino ${branch} running tests to check integration`)
   const integration = spawn('npm', ['test'], {cwd: join(REPOS, name), stdio: 'ignore'})
-  const integrated = await once(integration, 'close') === 0
+  const integrated = ~~(await once(integration,'close')) === 0
   if (integrated === false) {
     console.error(`Fail: ${name} failed integration test!`)
     process.exitCode = 1
