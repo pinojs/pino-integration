a2b4b2c118f6330974e16f58c9682f91b9ff38be
davidmarkclements <huperekchuno@googlemail.com>
2018-07-04 13:24:20 +0200

    run tests sequentially

--- start.js
+++ start.js
@@ -5,6 +5,7 @@ const { execSync, spawn, spawnSync } = require('child_process')
 const branch = process.env.TRAVIS_BRANCH || execSync('git rev-parse --abbrev-ref HEAD').toString().trim()
 const { print } = require('easy-table')
 const { pino, peers } = require('./config')
+const values = Object.values || ((o) => Object.keys(o).map((k) => o[k]))
 const REPOS = join(__dirname, 'repos')
 try { execSync(`rm -rf ${REPOS}`) } catch (e) {}
 mkdirSync(REPOS)
@@ -15,30 +16,29 @@ execSync(`git checkout ${branch}`, {cwd: join(REPOS, 'pino')}, {stdio: 'ignore'}
 const results = peers.reduce((o, {name}) => Object.assign({
   [name]: { library: name, current: false, branch: false, compare: ''}  
 }, o), {})
-var count = peers.length
-const values = Object.values || ((o) => Object.keys(o).map((k) => o[k]))
-peers.forEach(async ({name, url, divergent}) => {
-  await check(name, url, divergent)
-  count--
-  if (count === 0) {
-    const table = values(results).map(({library, current, branch, compare}) => ({
-      Library: library, 
-      Current: current ? '   ✅' : '   ❌', 
-      Branch: branch ? ' ✅' : ' ❌',
-      'Branch Test Comparison': compare
-    }))
-    console.log()
-    console.log(print(table))
+
+start()
+
+async function start () {
+  const master = await Promise.all(peers.map(setup))
+  for (const {name, divergent, fail} of master) {
+    if (fail === false) await test({name, divergent}) 
   }
-})
-async function check (name, url, divergent) {
+  const branch = await Promise.all(peers.map(switchAndLink))
+  for (const {name, divergent, fail} of branch) {
+    if (fail === false) await integration({name, divergent}) 
+  }
+  display()
+}
+
+async function setup ({name, url, divergent}) {
   console.log(`cloning ${name} repository`)
   const clone = spawn('git', ['clone', url])
   const cloneSuccess = ~~(await once(clone, 'close')) === 0
   if (cloneSuccess === false) {
     console.error(`Fail: Unable to clone ${name}`)
     process.exitCode = 1
-    return
+    return {name, divergent, fail: true}
   }
   console.log(`${name} cloned, installing dependencies`)
   const install = spawn('npm', ['install'], {cwd: join(REPOS, name), stdio: 'ignore'})
@@ -46,16 +46,22 @@ async function check (name, url, divergent) {
   if (installed === false) {
     console.error(`Fail: ${name} unable to install dependencies`)
     process.exitCode = 1
-    return
+    return {name, divergent, fail: true}
   }
-  console.log(`${name} dependencies installed, running current tests`)
+  
+  return {name, divergent, fail: false}
+}
 
+async function test ({name, divergent}) {
   const test = spawn('npm', divergent ? ['test'] : ['test', '--', '--timeout=0'], {cwd: join(REPOS, name), stdio: 'inherit'})
   const tested = ~~(await once(test, 'close')) === 0
   if (tested === false) {
     console.error(`Fail: ${name} failed current test!`)
     process.exitCode = 1
   } else results[name].current = true
+}
+
+async function switchAndLink({name, divergent}) {
   console.log(`${name} attempt switch to equivalent branch`)
   const checkout = await spawn('git', ['checkout', branch], {cwd: join(REPOS, name), stdio: 'ignore'})
   await once(checkout, 'close')
@@ -68,14 +74,14 @@ async function check (name, url, divergent) {
     if (modulesRemoved === false) {
       console.error(`Fail: ${name} unable to reinstall dependencies for ${repoBranch}, could not remove node_modules`)
       process.exitCode = 1
-      return
+      return {name, divergent, fail: true}
     }
     const reinstall = spawn('npm', ['install'], {cwd: join(REPOS, name), stdio: 'ignore'})
     const reinstalled = ~~(await once(reinstall, 'close')) === 0
     if (reinstalled === false) {
       console.error(`Fail: ${name} unable to reinstall dependencies for ${repoBranch}`)
       process.exitCode = 1
-      return
+      return {name, divergent, fail: true}
     }
     console.log(`${name} dependencies reinstalled, linking pino ${branch}`)
   } else {
@@ -86,8 +92,12 @@ async function check (name, url, divergent) {
   if (linked === false) {
     console.error(`Fail: ${name} could not link pino ${branch}!`)
     process.exitCode = 1
-    return
+    return {name, divergent, fail: true}
   }
+  return {name, divergent, fail: false}
+}
+
+async function integration ({name, divergent}) {
   console.log(`${name} linked to pino ${branch} running tests to check integration`)
   const integration = spawn('npm', divergent ? ['test'] : ['test', '--', '--timeout=0'], {cwd: join(REPOS, name), stdio: 'inherit'})
   const integrated = ~~(await once(integration,'close')) === 0
@@ -99,6 +109,18 @@ async function check (name, url, divergent) {
   results[name].branch = true
   console.log(`Success: ${name} passed integration test`)
 }
+
+function display () {
+  const table = values(results).map(({library, current, branch, compare}) => ({
+    Library: library, 
+    Current: current ? '   ✅' : '   ❌', 
+    Branch: branch ? ' ✅' : ' ❌',
+    'Branch Test Comparison': compare
+  }))
+  console.log()
+  console.log(print(table))
+}
+
 
 function once (emitter, name) {
   return new Promise((resolve, reject) => {
